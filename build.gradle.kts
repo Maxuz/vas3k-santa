3e8bfff9b67c67d906f34b9f678b1b1a70fd2fc5
maxuz <16779208+Maxuz@users.noreply.github.com>
2023-10-08 12:25:39 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("idea")
+}
+
+group = "dev.maxuz"
+version = "1.0-SNAPSHOT"
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
